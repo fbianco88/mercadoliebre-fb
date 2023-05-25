1c2fd082876fa88d302677e4be761c554ac1ace9
Fermín Bianco <ferminbianco40@gmail.com>
2023-05-24 21:16:21 -0300

    subo proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.static('public'))
+
+app.listen(process.env.PORT || 3000,() => {
+    console.log("Servidos corriendo en el puerto 3000")
+});
+
+app.get("/",(req,res) => {
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+})
